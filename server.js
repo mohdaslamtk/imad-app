654795f30971bd2a500ca8b91421e6dd809c8246
mohdaslamtk <30614846+mohdaslamtk@users.noreply.github.com>
2017-08-27 12:14:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,6 +84,12 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
+var countr=0;
+app.get('/counter',function (req,res){
+    countrer=counter+1;
+    res.send(counter.toString());
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
