a989bb0e32fb606fe26c72b984a420a7244a9b52
mohdaslamtk <30614846+mohdaslamtk@users.noreply.github.com>
2017-08-27 13:44:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-var countr=0;
+var counter=0;
 app.get('/counter',function (req,res){
     countrer=counter+1;
     res.send(counter.toString());
